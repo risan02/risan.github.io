c9572d1c512d231eabdfd565a4de5600a144eccf
Jack <risan2002@163.com>
2025-09-18 00:10:30 +0800

    Initial commit: Valaxy blog project

---
+++ site.config.ts
@@ -0,0 +1,95 @@
+import { defineSiteConfig } from 'valaxy'
+
+export default defineSiteConfig({
+  url: 'httpd://risann.cn',  //你网站的URL
+  favicon: "",	// 网页图标链接
+  lang: 'zh-CN',  //默认语言
+  title: "Risan's Blog | Risan的小站",  //网站标题
+  subtitle: 'Be water my friend',//网站副标题
+  author: {
+    name: 'Risan',//博主名称
+    avatar: "/images/avatar01.jpg",	//头像链接
+    status: {
+      emoji: '🤔'	,// 头像旁边的emoji
+      message: 'emmmm'
+    },
+  },
+  
+  description: "Risan's blog",  //简介
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',  //这个是博客自带的RSS订阅，尽量留着，方便其他博友为你订阅
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'GitHub',
+      link: '',  //这里填写你的GitHub地址，不需要的话删除此字段即可
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    {
+      name: '网易云音乐',
+      link: '',  //这里填写你的网易云音乐地址，不需要的话删除此字段即可
+      icon: 'i-ri-netease-cloud-music-line',
+      color: '#C20C0C',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/268619071?spm_id_from=333.788.0.0',  //这里填写你的BiliBili地址，不需要的话删除此字段即可
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    {
+      name: 'Twitter',
+      link: '',  //这里填写你的Twitter地址，不需要的话删除此字段即可
+      icon: 'i-ri-twitter-x-fill',
+      color: 'black',
+    },
+    {
+      name: 'E-Mail',
+      link: 'mailto:risan2002@163.com', //这里在mailto后面填写你的Email地址，不需要的话删除此字段即可
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+  ],
+
+  search: {
+    enable: true,
+  },
+    comment: {
+      enable: true
+    },
+    statistics: {
+      enable: true,
+      readTime: {
+        /**
+         * 阅读速度
+         */
+        speed: {
+          cn: 300,
+          en: 200,
+        },
+      },
+    },
+
+  sponsor: {
+    enable: true,
+    title: '我很可爱，请给我钱！',
+    methods: [
+      {
+        name: '支付宝',
+        url: '', //这里填写你的支付宝收款码图片链接
+        color: '#00A3EE',
+        icon: 'i-ri-alipay-line',
+      },
+      {
+        name: '微信支付',
+        url: '',//这里填写你的微信收款码图片链接
+        color: '#2DC100',
+        icon: 'i-ri-wechat-pay-line',
+      },
+    ],
+  },
+})
---
+++ valaxy.config.ts
@@ -0,0 +1,93 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+//这两行均需要安装对应的插件，详情请查看Valaxy开发文档
+//import { addonTwikoo } from 'valaxy-addon-twikoo'  //接入Twikoo评论系统
+//import { addonMeting } from 'valaxy-addon-meting'  //添加Meting音乐播放器
+// add icons what you will need
+
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '無限进步',
+    },
+    pages: [
+      {
+        name: '网络世界的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-open-arm-line',  //这里的icon是Valaxy自带的图标，你可以在https://icones.js.org/找到你需要的图标，然后复制到icon字段中
+        //这里的ico我踩过坑，所以多说两句，这里的ICO复制名字即可，但是你需要在前面添加i-ri-【ICO名字】
+        color: 'hotpink',
+      },
+      {
+        name: '感情故事',  //这个girls文件夹默认是没有的，你需要自己在pages里创建，并在girls文件夹里创建index.md文件
+        url: '/girls/',
+        icon: 'i-ri-heart-3-line',
+        color: 'hotpink',
+      },
+      {
+        name: '分类',
+        url: '/categories/',
+        icon: 'i-ri-apps-line',
+        color: 'dodgerblue',
+      },
+      {
+        name: '标签',
+        url: '/tags/',
+        icon: 'i-ri-bookmark-3-line',
+        color: 'dodgerblue',
+      },
+    ],
+    colors: {
+      primary: "#D69B54",
+    },
+    //页脚
+    footer: {
+      since: 2023,
+      powered: true, //这里是显示Valaxy驱动信息的，尊重作者劳动成果，我选择开启
+      beian: {
+        enable: true,
+        icp: '',  //这里是备案号，如果你不需要备案号，可以将上面的enable改为false即可
+      },
+    },
+    
+    //背景图,这里为我自己添加的字段
+    bg_image: {
+      enable: false,  //这里是背景图的设置，你可以设置白日模式和夜间模式的背景图，如果你不需要背景图，可以将上面的enable改为false即可
+      url: "",	// 白日模式背景
+      dark: "",	// 夜间模式背景
+    },
+
+     //鼠标点击烟花特效
+    fireworks: {  
+      enable: true,
+      colors: ['#FFE57D', '#FFCD88', '#E6F4AD']
+    },
+
+  },
+  unocss: { safelist },
+  siteConfig: {
+    // 启用评论
+    comment: {
+      enable: true  //这里是评论的设置，如果你不需要评论，可以将enable改为false即可
+    },
+  },
+  // 设置 valaxy-addon-twikoo 配置项
+  //addons: [
+  //  addonTwikoo({ 
+  //    envId: '', // 自建服务地址
+  //  }),
+  //],
+})
